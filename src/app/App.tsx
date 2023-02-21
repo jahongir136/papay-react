cc1366bd48da8728df0148cedaef22bae77be160
jahongir136 <rasuljonov_96@naver.com>
2023-02-21 19:56:52 +0900

    feat: develop header navbarHome component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import "../css/App.css";
+import "../css/navbar.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { RestaurantPage } from "./screens/RestaurantPage ";
 import { CommunityPage } from "./screens/CommunityPage ";
@@ -9,59 +10,72 @@ import { MemberPage } from "./screens/MemberPage";
 import { HelpPage } from "./screens/HelpPage";
 import { LoginPage } from "./screens/LoginPage";
 import { Homepage } from "./screens/Homepage";
+import { NavbarHome } from "./components/header";
+import { NavbarRestaurant } from "./components/header/restaurant";
+import { NavbarOthers } from "./components/header/others";
 
 function App() {
+  const [path, setPath] = useState();
+  const main_path = window.location.pathname;
   return (
     <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/restaurant">RestaurantPage</Link>
-          </li>
-          <li>
-            <Link to="/community">CommunityPage</Link>
-          </li>
-          <li>
-            <Link to="/orders">OrdersPage</Link>
-          </li>
-          <li>
-            <Link to="/member-page">MemberPage</Link>
-          </li>
-          <li>
-            <Link to="/help">HelpPage</Link>
-          </li>
-          <li>
-            <Link to="/login">LoginPage</Link>
-          </li>
-          <li>
-            <Link to="/">Homepage</Link>
-          </li>
-        </ul>
+      {main_path == "/" ? (
+        <NavbarHome setPath={setPath} />
+      ) : main_path.includes("/restaurant") ? (
+        <NavbarRestaurant setPath={setPath} />
+      ) : (
+        <NavbarOthers setPath={setPath} />
+      )}
 
-        <Switch>
-          <Route path="/restaurant">
-            <RestaurantPage />
-          </Route>
-          <Route path="/community">
-            <CommunityPage />
-          </Route>
-          <Route path="/orders">
-            <OrdersPage />
-          </Route>
-          <Route path="/member-page">
-            <MemberPage />
-          </Route>
-          <Route path="/help">
-            <HelpPage />
-          </Route>
-          <Route path="/login">
-            <LoginPage />
-          </Route>
-          <Route path="/">
-            <Homepage />
-          </Route>
-        </Switch>
-      </div>
+      {/* <nav>
+          <ul>
+            <li>
+              <Link to="/restaurant">RestaurantPage</Link>
+            </li>
+            <li>
+              <Link to="/community">CommunityPage</Link>
+            </li>
+            <li>
+              <Link to="/orders">OrdersPage</Link>
+            </li>
+            <li>
+              <Link to="/member-page">MemberPage</Link>
+            </li>
+            <li>
+              <Link to="/help">HelpPage</Link>
+            </li>
+            <li>
+              <Link to="/login">LoginPage</Link>
+            </li>
+            <li>
+              <Link to="/">Homepage</Link>
+            </li>
+          </ul>
+        </nav> */}
+
+      <Switch>
+        <Route path="/restaurant">
+          <RestaurantPage />
+        </Route>
+        <Route path="/community">
+          <CommunityPage />
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <MemberPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage />
+        </Route>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/">
+          <Homepage />
+        </Route>
+      </Switch>
     </Router>
   );
 }
---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,116 @@
+import {
+  Badge,
+  Button,
+  ButtonBase,
+  Container,
+  IconButton,
+  Stack,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export function NavbarHome(props: any) {
+  return (
+    <div className="format home_navbar">
+      <Container>
+        <Stack
+          flexDirection={"row"}
+          className="navbar_config"
+          justifyContent={"space-between"}
+        >
+          <Box>
+            <img src="/icons/papay.svg" />
+          </Box>
+          <Stack
+            flexDirection={"row"}
+            justifyContent="space-evenly"
+            alignItems={"center"}
+            className="navbar_links"
+          >
+            <Box className="hover-line" onClick={props.setPath}>
+              <NavLink to="/" activeClassName="underline">
+                Bosh sahifa
+              </NavLink>
+            </Box>
+            <Box className="hover-line" onClick={props.setPath}>
+              <NavLink to="/restaurant" activeClassName="underline">
+                Oshxona
+              </NavLink>
+            </Box>
+            <Box className="hover-line" onClick={props.setPath}>
+              <NavLink to="/orders" activeClassName="underline">
+                Buyurtma
+              </NavLink>
+            </Box>
+            <Box className="hover-line" onClick={props.setPath}>
+              <NavLink to="/community" activeClassName="under-line">
+                Jamiyat
+              </NavLink>
+            </Box>
+            <Box className="hover-line" onClick={props.setPath}>
+              <NavLink to="/help" activeClassName="underline">
+                Yordam
+              </NavLink>
+            </Box>
+
+            <Box className="hover-line">
+              <IconButton
+                aria-label="cart"
+                id="basic-button"
+                aria-controls={undefined}
+                aria-hospopup="true"
+                aria-expanded={undefined}
+                // onClick={handleClick}
+              >
+                <Badge badgeContent={3} color="secondary">
+                  <img src={"/icons/shopping_cart.svg"} />
+                </Badge>
+              </IconButton>
+            </Box>
+            <Box>
+              <Button
+                variant="contained"
+                style={{ color: " #ffffff", background: "#1976d2" }}
+              >
+                KIRISH
+              </Button>
+            </Box>
+          </Stack>
+        </Stack>
+
+        <Stack className="head_information" justifyContent={"row"}>
+          <Stack
+            justifyContent={"column"}
+            style={{ marginTop: "86px", marginLeft: "24px" }}
+          >
+            <Box>
+              {" "}
+              <img src="/icons/wellcome.svg" />
+            </Box>
+            <Box className="define_restaurant">
+              The Authentic Restaurant & Cafe
+            </Box>
+            <Box className="timeline_service">24 soat xizmatdamiz.</Box>
+            <Box sx={{ mt: "90px" }}>
+              <Button
+                variant="contained"
+                style={{
+                  width: "210px",
+                  height: "60px",
+                  background: "#1976d2",
+                  color: "#ffffff",
+                }}
+              >
+                RO'YHATDAN O'TISH
+              </Button>
+            </Box>
+          </Stack>
+          <Stack flexDirection={"column"}>
+            <Box className="big_img"></Box>
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
+}
