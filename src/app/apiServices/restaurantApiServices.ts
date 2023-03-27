f92f012cad966b894c0b4ee795b8fc86854a89f2
jahongir136 <rasuljonov_96@naver.com>
2023-03-28 03:38:06 +0900

    feat: retrive top restaurants data

---
+++ src/app/apiServices/restaurantApiServices.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import assert from "assert";
+import { serviceApi } from "../../lib/config";
+import { Definer } from "../../lib/Definer";
+import { Restaurant } from "../../types/user";
+
+class RestaurantApiServices {
+  private readonly path: string;
+
+  constructor() {
+    this.path = serviceApi;
+  }
+  async getTopRestaurants() {
+    try {
+      const url = "/restaurants?order=best&page=1&limit=4",
+        result = await axios.get(this.path + url, { withCredentials: true });
+      assert.ok(result, Definer.general_err1);
+
+      console.log("state::", result.data.state);
+      const top_restaurants: Restaurant[] = result.data.data;
+      return top_restaurants;
+    } catch (err: any) {
+      console.log(`ERROR ::: getTopRestaurants ${err.message}`);
+      throw err;
+    }
+  }
+}
+export default RestaurantApiServices;
--- src/app/screens/Homepage/index.tsx
+++ src/app/screens/Homepage/index.tsx
@@ -16,6 +16,7 @@ import { createSelector } from "reselect";
 import { setTopRestaurants } from "../../screens/Homepage/slice";
 import { retrievetopRestaurants } from "../../screens/Homepage/selector";
 import { Restaurant } from "../../../types/user";
+import RestaurantApiServices from "../../apiServices/restaurantApiServices";
 
 /** REDUX SLICE */
 const actionDispatch = (dispach: Dispatch) => ({
@@ -33,35 +34,16 @@ const topRestaurantRetriever = createSelector(
 export function Homepage() {
   /** INITIALLIZATION */
   const { setTopRestaurants } = actionDispatch(useDispatch());
-  const { topRestaurants } = useSelector(topRestaurantRetriever);
-
-  console.log("topRestaurants::", topRestaurants);
 
   useEffect(() => {
     // backend data request => data
-    const data: Restaurant[] = [
-      {
-        _id: "6409bc23911a2da370171245",
-        mb_nick: "Nusret",
-        mb_phone: "9988932892",
-        mb_type: "RESTAURANT",
-        mb_status: "ACTIVE",
-        mb_image: "uploads/members/840bc2ac-f547-46fc-8e2d-9d172d98fbb1.jpeg",
-        mb_point: 0,
-        mb_top: "N",
-        mb_views: 3,
-        mb_likes: 0,
-        mb_follow_cnt: 3,
-        mb_subscriber_cnt: 2,
-        me_liked: [
-          {
-            mb_id: "6412dc2c6602c025db8e1a2d",
-            like_ref_id: "6409bc23911a2da370171245",
-            my_favorite: true,
-          },
-        ],
-      },
-    ];
+    const restaurantServices = new RestaurantApiServices();
+    restaurantServices
+      .getTopRestaurants()
+      .then((data) => {
+        setTopRestaurants(data);
+      })
+      .catch((err) => console.log(err));
     setTopRestaurants([]);
     // slice: data => store
   }, []);
