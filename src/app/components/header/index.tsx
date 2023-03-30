7f9f9b31a604180b7ad091ff744ac7b358e3eb94
jahongir136 <rasuljonov_96@naver.com>
2023-03-30 13:06:07 +0900

    feat: develop login authentication process

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -80,6 +80,7 @@ export function NavbarHome(props: any) {
               <Button
                 variant="contained"
                 style={{ color: " #ffffff", background: "#1976d2" }}
+                onClick={props.handleLoginOpen}
               >
                 KIRISH
               </Button>
@@ -109,7 +110,7 @@ export function NavbarHome(props: any) {
                   background: "#1976d2",
                   color: "#ffffff",
                 }}
-                onClick={() => setValue(!value)}
+                onClick={props.handleSignUpOpen}
               >
                 RO'YHATDAN O'TISH
               </Button>
