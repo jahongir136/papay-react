5a5b2baff9d850dc8a4799369e972213e74218fb
jahongir136 <rasuljonov_96@naver.com>
2023-04-06 18:03:59 +0900

    fix: refactoring error assertions

--- src/app/apiServices/productApiService.ts
+++ src/app/apiServices/productApiService.ts
@@ -18,8 +18,8 @@ class ProductApiService {
         result = await axios.post(this.path + url, data, {
           withCredentials: true,
         });
-      assert.ok(result, Definer.general_err1);
-
+      assert.ok(result?.data, Definer.general_err1);
+      assert.ok(result?.data?.state != "fail", result?.data?.message);
       console.log("state:", result.data.state);
       const products: Product[] = result.data.data;
       return products;
@@ -35,8 +35,8 @@ class ProductApiService {
         result = await axios.get(this.path + url, {
           withCredentials: true,
         });
-      assert.ok(result, Definer.general_err1);
-
+      assert.ok(result?.data, Definer.general_err1);
+      assert.ok(result?.data?.state != "fail", result?.data?.message);
       console.log("state:", result.data.state);
       const product: Product = result.data.data;
       return product;
--- src/app/apiServices/restaurantApiService.ts
+++ src/app/apiServices/restaurantApiService.ts
@@ -15,9 +15,9 @@ class RestaurantApiServices {
     try {
       const url = "/restaurants?order=top&page=1&limit=4",
         result = await axios.get(this.path + url, { withCredentials: true });
-      assert.ok(result, Definer.general_err1);
-
-      console.log("state::", result.data.state);
+      assert.ok(result?.data, Definer.general_err1);
+      assert.ok(result?.data?.state != "fail", result?.data?.message);
+      console.log("state:", result.data.state);
       const top_restaurants: Restaurant[] = result.data.data;
       return top_restaurants;
     } catch (err: any) {
@@ -30,9 +30,9 @@ class RestaurantApiServices {
     try {
       const url = `/restaurants?order=${data.order}&page=${data.page}&limit=${data.limit}`,
         result = await axios.get(this.path + url, { withCredentials: true });
-      assert.ok(result, Definer.general_err1);
-
-      console.log("state::", result.data.state);
+      assert.ok(result?.data, Definer.general_err1);
+      assert.ok(result?.data?.state != "fail", result?.data?.message);
+      console.log("state:", result.data.state);
       const restaurants: Restaurant[] = result.data.data;
       return restaurants;
     } catch (err: any) {
@@ -44,9 +44,9 @@ class RestaurantApiServices {
     try {
       const url = `/restaurants/${id}`,
         result = await axios.get(this.path + url, { withCredentials: true });
-
-      assert.ok(result, Definer.general_err1);
-      console.log("state", result.data.data);
+      assert.ok(result?.data, Definer.general_err1);
+      assert.ok(result?.data?.state != "fail", result?.data?.message);
+      console.log("state:", result.data.state);
       const restaurant: Restaurant = result.data.data;
       return restaurant;
     } catch (err: any) {
