f88d7a33bccff7a902fb0107b4e02b5351f80fa3
jahongir136 <rasuljonov_96@naver.com>
2023-04-21 01:32:12 +0900

    fix: CommunityPage  develop chat based on  client socket.io

--- src/app/screens/CommunityPage /communityChats.tsx
+++ src/app/screens/CommunityPage /communityChats.tsx
@@ -1,14 +1,60 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { Container, Avatar, Box, Stack, Button } from "@mui/material";
 import "../../../css/community.css";
 import SendIcon from "@mui/icons-material/Send";
 import { SocketContext } from "../../context/socket";
+import { ChatGreetMsg, ChatInfoMsg, ChatMessage } from "../../../types/others";
+import { verifiedMemberData } from "../../apiServices/verify";
+import {
+  sweetErrorHandling,
+  sweetFailureProvider,
+} from "../../../lib/sweetAlert";
+import assert from "assert";
+import { Definer } from "../../../lib/Definer";
+import { RippleBadge } from "../../MaterialTheme/styled";
+
+const NewMessage = (data: any) => {
+  if (data.new_message.mb_id == verifiedMemberData?._id) {
+    return (
+      <Box
+        flexDirection={"row"}
+        style={{ display: "flex" }}
+        alignItems={"flex-end"}
+        sx={{ m: "10px 0px " }}
+      >
+        <div className={"msg_right"}>{data.new_message.msg}</div>
+      </Box>
+    );
+  } else {
+    return (
+      <Box
+        flexDirection={"row"}
+        style={{ display: "flex" }}
+        sx={{ m: "10px 0px" }}
+      >
+        <Avatar
+          alt={data.new_message.mb_nick}
+          src={data.new_message.mb_image}
+        />
+        <div className={"msg_left"}>{data.new_message.msg} </div>
+      </Box>
+    );
+  }
+};
 
 export function CommunityChats() {
   /**INITIALIZATIONS */
   const [messagesList, setMessageList] = useState([]);
   const socket = useContext(SocketContext);
   const [onlineUsers, setOnlineUsers] = useState<number>(0);
+  const textInput: any = useRef(null);
+  const [message, setMessage] = useState<string>("");
 
   useEffect(() => {
     socket.connect();
@@ -18,15 +64,33 @@ export function CommunityChats() {
       console.log("CLIENT: connected");
     });
 
-    socket?.on("newMsg", (new_message: any) => {
+    socket?.on("newMsg", (new_message: ChatMessage) => {
       console.log("CLIENT: new message");
+      messagesList.push(
+        // @ts-ignore
+        <NewMessage new_message={new_message} key={messagesList.length} />
+      );
+      setMessageList([...messagesList]);
     });
 
-    socket?.on("greetMsg", (new_message: any) => {
+    socket?.on("greetMsg", (msg: ChatGreetMsg) => {
       console.log("CLIENT: greet message");
+      messagesList.push(
+        // @ts-ignore
+        <p
+          style={{
+            textAlign: "center",
+            fontSize: "large",
+            fontFamily: "serif",
+          }}
+        >
+          {msg.text}, dear {verifiedMemberData?.mb_nick ?? "guest"}
+        </p>
+      );
+      setMessageList([...messagesList]);
     });
 
-    socket?.on("infoMsg", (msg: any) => {
+    socket?.on("infoMsg", (msg: ChatInfoMsg) => {
       console.log("CLIENT: info message");
       setOnlineUsers(msg.total);
     });
@@ -36,9 +100,60 @@ export function CommunityChats() {
     };
   }, [socket]);
 
+  /** HANDLER */
+  const getInputMessageHandler = useCallback(
+    (e: any) => {
+      const text = e.target.value;
+      setMessage(text);
+    },
+    [message]
+  );
+
+  const getKeyHandler = (e: any) => {
+    try {
+      if (e.key == "Enter") {
+        assert.ok(message, Definer.input_err3);
+        onClickHandler();
+      }
+    } catch (err: any) {
+      sweetErrorHandling(err).then();
+    }
+  };
+
+  const onClickHandler = () => {
+    try {
+      if (!verifiedMemberData) {
+        (textInput.current.value = ""),
+          sweetFailureProvider("Please login first", true);
+        return false;
+      }
+
+      (textInput.current.value = ""), assert.ok(message, Definer.input_err3);
+
+      const mb_image_url =
+        verifiedMemberData?.mb_image ?? "/auth/default_user.svg";
+
+      socket.emit("createMsg", {
+        msg: message,
+        mb_id: verifiedMemberData?._id,
+        mb_nick: verifiedMemberData?.mb_nick,
+        mb_image: mb_image_url,
+      });
+      setMessage("");
+    } catch (err: any) {
+      console.log("onClickHandler, Error:", err);
+      sweetErrorHandling(err).then();
+    }
+  };
   return (
     <Stack className={"chat_frame"}>
-      <Box className={"chat_top"}>Jonli Muloqot {onlineUsers} </Box>
+      <Box className={"chat_top"}>
+        <div>Jonli Muloqot</div>
+        <RippleBadge
+          style={{ margin: "-30px 0 0 20px" }}
+          badgeContent={onlineUsers}
+        />
+      </Box>
       <Box className={"chat_content"}>
         <Stack className={"chat_main"}>
           <Box
@@ -48,32 +163,20 @@ export function CommunityChats() {
           >
             <div className={"msg_left"}>buyerda Jonli muloqot </div>
           </Box>
-          <Box
-            flexDirection={"row"}
-            style={{ display: "flex" }}
-            alignItems={"flex-end"}
-            sx={{ m: "10px 0px " }}
-          >
-            <div className={"msg_right"}>Assalomalekum yaxshimsz</div>
-          </Box>
-          <Box
-            flexDirection={"row"}
-            style={{ display: "flex" }}
-            sx={{ m: "10px 0px" }}
-          >
-            <Avatar alt={"martin"} src={"/community/cute_girl.jpeg"} />
-            <div className={"msg_left"}>Volekum assalom raxmat </div>
-          </Box>
+          {messagesList}
         </Stack>
       </Box>
       <Box className={"chat_bott"}>
         <input
+          ref={textInput}
           type={"text"}
           name={"message"}
           className={"msg_input"}
           placeholder={"xabar jo'natish"}
+          onChange={getInputMessageHandler}
+          onKeyDown={getKeyHandler}
         />
-        <button className={"send_msg_btn"}>
+        <button className={"send_msg_btn"} onClick={onClickHandler}>
           <SendIcon style={{ color: "#fff" }} />
         </button>
       </Box>
